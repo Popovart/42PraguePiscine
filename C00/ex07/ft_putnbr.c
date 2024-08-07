bc5d6fd64acda8b24ea2c6aa07deb3e6a42ce675
Dmitrii Popov <dpopov@c2r1s5.42prague.com>
2024-08-07 20:33:51 +0200

    lol kek3

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,19 +6,17 @@
 /*   By: dpopov <dpopov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 20:03:28 by dpopov            #+#    #+#             */
-/*   Updated: 2024/08/07 20:14:13 by dpopov           ###   ########.fr       */
+/*   Updated: 2024/08/07 20:33:15 by dpopov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 
-void ft_putnbr(int nb){
-    char buffer[12];
-    int length = snprintf(buffer, sizeof(buffer), "%d", nb);  
-    write(1, buffer, length);
-}
+void	ft_putnbr(int nb)
+{
+	char	buffer[12];
+	int		length;
 
-int main() {
-    ft_putnbr(42);
-    return 0;
+	length = snprintf(buffer, sizeof(buffer), "%d", nb);
+	write(1, buffer, length);
 }
